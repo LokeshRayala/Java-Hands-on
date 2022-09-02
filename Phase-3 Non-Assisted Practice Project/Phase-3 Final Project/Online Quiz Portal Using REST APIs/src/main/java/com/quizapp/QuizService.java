04bb36929d89ace0bb1c844628087ab1bc7dd8a3
Lokesh Rayala <72343510+LokeshRayala@users.noreply.github.com>
2022-09-02 10:35:47 +0530

    Phase-3 Final Project

---
+++ Phase-3 Non-Assisted Practice Project/Phase-3 Final Project/Online Quiz Portal Using REST APIs/src/main/java/com/quizapp/QuizService.java
@@ -0,0 +1,20 @@
+package com.quizapp;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class QuizService<questionsid> {
+	
+	@Autowired
+	private QuizRepo repo;
+	
+	public List<QuizCompetition> findque(int quizid) {
+		List<QuizCompetition> questionss = repo.findByQuizid(quizid);
+		return questionss;
+	}
+
+}
